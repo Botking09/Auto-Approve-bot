fc84e8a7a316711d4aefc7feac37715581cbda49
Botking09 <156010397+Botking09@users.noreply.github.com>
2024-01-12 09:55:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,6 +7,6 @@ class Config:
     FSUB = getenv("FSUB", "SDBotz")
     CHID = int(getenv("CHID", "-1000112234"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Ankitking:Ankitking@cluster0.oyrtdsm.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
