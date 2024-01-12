f3b20e4f6afc43e6822ccd33e298f11f6bb1b4fc
Botking09 <156010397+Botking09@users.noreply.github.com>
2024-01-12 10:00:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,11 +1,11 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
+    API_ID = int(getenv("API_ID", "27514811"))
+    API_HASH = getenv("API_HASH", "88b650f1272ab3d38474b18d3bcc66a8")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
+    FSUB = getenv("FSUB", "https://thanosXbotz")
+    CHID = int(getenv("CHID", "-1001991743829"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Ankitking:Ankitking@cluster0.oyrtdsm.mongodb.net/?retryWrites=true&w=majority")
     
